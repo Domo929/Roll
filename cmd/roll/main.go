053b9a16e7d2c26049a0695f265fde6bffbc2954
Dominic <Dcupo@cogolabs.com>
2021-01-26 10:50:26 -0500

    Updated age rolling mechanics, added modifier

--- cmd/roll/main.go
+++ cmd/roll/main.go
@@ -11,20 +11,24 @@ import (
 )
 
 var (
-	race = flag.Bool("race", false, "Whether to roll 3d6 incl drama die")
+	age = flag.Bool("age", false, "Whether to roll 3d6 incl drama die")
 )
 
 const (
-	dramaFmt = "%d *\n"
-	normFmt  = "%d\n"
+	dramaFmt = "%d*"
+	normFmt  = "%d"
 )
 
 func main() {
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
 
-	if *race {
-		raceGen()
+	if len(flag.Args()) == 0 {
+		log.Fatal("need to provide 'age [+/-]modifier or a list of die rolls (3d6, 2d8, etc)")
+	}
+
+	if flag.Args()[0] == "age" {
+		ageGen(flag.Args())
 		return
 	}
 
@@ -35,19 +39,66 @@ func result(sides int) int {
 	return rand.Intn(sides) + 1
 }
 
-func raceGen() {
-	total := 0
-	for i := 0; i < 3; i++ {
-		die := result(6)
-		total += die
-
-		dieFmt := normFmt
-		if i == 0 {
-			dieFmt = dramaFmt
-		}
-		fmt.Printf(dieFmt, die)
+func ageGen(args []string) error {
+	modifier := "0"
+	if len(args) == 2 {
+		modifier = args[1]
 	}
-	fmt.Printf("Total: %d\n", total)
+	modVal, negative, err := parseModifier(modifier)
+	if err != nil {
+		return err
+	}
+
+	dies := make([]int, 0, 3)
+	sum := 0
+	for i := 0; i < 3; i++ {
+		res := result(6)
+
+		dies = append(dies, res)
+		sum += res
+	}
+	stuntPoints := dies[0] == dies[1] || dies[0] == dies[2] || dies[1] == dies[2]
+
+	dieValsStr := fmt.Sprint(dies[0])
+	for i := 1; i < len(dies); i++ {
+		dieValsStr += fmt.Sprintf(" %d", dies[i])
+	}
+	fmt.Printf("Dies: %s*\n", dieValsStr)
+
+	if negative {
+		sum -= modVal
+		fmt.Printf("Modifier: -%d\n", modVal)
+	} else {
+		sum += modVal
+		fmt.Printf("Modifier: +%d\n", modVal)
+	}
+
+	fmt.Println("Total: ", sum)
+	if stuntPoints {
+		fmt.Printf("Generated %d stunt points\n", dies[2])
+	}
+
+	return nil
+}
+
+func parseModifier(modifier string) (int, bool, error) {
+	var (
+		numIndexStart = 0
+		negative      = false
+	)
+
+	if len(modifier) >= 2 {
+		numIndexStart = 1
+		if modifier[0] == '-' {
+			negative = true
+		}
+	}
+	val, err := strconv.ParseInt(modifier[numIndexStart:], 10, 64)
+	if err != nil {
+		return 0, false, err
+	}
+
+	return int(val), negative, nil
 }
 
 func normGen(dieGens []string) {
@@ -66,11 +117,13 @@ func normGen(dieGens []string) {
 		}
 		fmt.Println(resMsg)
 	}
-	if len(dieGens) > 0 {
-		fmt.Println("total: ", total)
-	} else {
+
+	if len(dieGens) == 0 {
 		fmt.Println("no die combos provided")
+		return
 	}
+
+	fmt.Println("total: ", total)
 
 }
 
